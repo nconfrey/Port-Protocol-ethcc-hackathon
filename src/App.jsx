bcdfa867eb1e3d574e2b1ae1a67748d63ccf5b02
Nick Confrey <nickconfrey@earthlink.net>
2022-07-22 23:43:27 +0200

    refactor to use moralis wrapper around the portfolio and marketplace calls. transport over the alchemy minter which still works with the account connected through moralis

---
+++ src/App.jsx
@@ -0,0 +1,126 @@
+import { useEffect, useState} from "react";
+import { useMoralis } from "react-moralis";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+  Redirect,
+} from "react-router-dom";
+import Account from "components/Account";
+import Chains from "components/Chains";
+import NFTBalance from "components/NFTBalance";
+import NFTTokenIds from "components/NFTTokenIds";
+import { Menu, Layout} from "antd";
+import "antd/dist/antd.css";
+import NativeBalance from "components/NativeBalance";
+import "./style.css";
+import Text from "antd/lib/typography/Text";
+import Minter from "components/Minter"
+const { Header, Footer } = Layout;
+
+const styles = {
+  content: {
+    display: "flex",
+    justifyContent: "center",
+    fontFamily: "Roboto, sans-serif",
+    color: "#041836",
+    marginTop: "130px",
+    padding: "10px",
+  },
+  header: {
+    position: "fixed",
+    zIndex: 1,
+    width: "100%",
+    background: "#fff",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    fontFamily: "Roboto, sans-serif",
+    borderBottom: "2px solid rgba(0, 0, 0, 0.06)",
+    padding: "0 10px",
+    boxShadow: "0 1px 10px rgb(151 164 175 / 10%)",
+  },
+  headerRight: {
+    display: "flex",
+    gap: "20px",
+    alignItems: "center",
+    fontSize: "15px",
+    fontWeight: "600",
+  },
+};
+const App = ({ isServerInfo }) => {
+  const { isWeb3Enabled, enableWeb3, isAuthenticated, isWeb3EnableLoading } =
+    useMoralis();
+
+  const [inputValue, setInputValue] = useState("explore");
+
+  useEffect(() => {
+    if (isAuthenticated && !isWeb3Enabled && !isWeb3EnableLoading) enableWeb3();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuthenticated, isWeb3Enabled]);
+
+  return (
+    <Layout style={{ height: "100vh", overflow: "auto" }}>
+      <Router>
+        <Header style={styles.header}>
+          <Logo />
+          <Menu
+            theme="light"
+            mode="horizontal"
+            style={{
+              display: "flex",
+              fontSize: "17px",
+              fontWeight: "500",
+              marginLeft: "50px",
+              width: "100%",
+            }}
+            defaultSelectedKeys={["feed"]}
+          >
+            <Menu.Item key="feed" onClick={() => setInputValue("explore")} >
+              <NavLink to="/feed">Feed</NavLink>
+            </Menu.Item>
+            <Menu.Item key="portfolio">
+              <NavLink to="/portfolio">Your Portfolio</NavLink>
+            </Menu.Item>
+            <Menu.Item key="minter">
+              <NavLink to="/minter">Mint New</NavLink>
+            </Menu.Item>
+          </Menu>
+          <div style={styles.headerRight}>
+            <Chains />
+            <NativeBalance />
+            <Account />
+          </div>
+        </Header>
+        <div style={styles.content}>
+          <Switch>
+            <Route path="/portfolio">
+              <NFTBalance />
+            </Route>
+            <Route path="/feed">
+              <NFTTokenIds inputValue={inputValue} setInputValue={setInputValue}/>
+            </Route>
+            <Route path="/minter">
+              <Minter />
+            </Route>
+          </Switch>
+          <Redirect to="/feed" />
+        </div>
+      </Router>
+      <Footer style={{ textAlign: "center" }}>
+        <Text style={{ display: "block" }}>
+          Built for the EthCC Hackathon
+        </Text>
+      </Footer>
+    </Layout>
+  );
+};
+
+export const Logo = () => (
+  <div style={{ display: "flex" }}>
+    <Text>Port</Text>
+  </div>
+);
+
+export default App;
