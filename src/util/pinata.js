8d6ac7a1d653f320d4e183bf9fe98eed14b7e3ca
Nick Confrey <nickconfrey@earthlink.net>
2022-07-22 19:49:36 +0200

    reformat pinning metadata to ipfs

--- src/util/pinata.js
+++ src/util/pinata.js
@@ -3,27 +3,28 @@ const key = process.env.REACT_APP_PINATA_KEY;
 const secret = process.env.REACT_APP_PINATA_SECRET;
 const axios = require('axios');
 
-export const pinJSONToIPFS = async(JSONBody) => {
-    const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
-    return axios
-        .post(url, JSONBody, {
-            headers: {
-                pinata_api_key: key,
-                pinata_secret_api_key: secret,
-            }
-        })
-        .then(function (response) {
-           return {
-               success: true,
-               pinataUrl: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
-           };
-        })
-        .catch(function (error) {
-            console.log(error)
-            return {
-                success: false,
-                message: error.message,
-            }
-           
-        });
+export const pinJSONToIPFS = async (JSONBody) => {
+  const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+  return axios
+    .post(url, JSONBody, {
+      headers: {
+        pinata_api_key: key,
+        pinata_secret_api_key: secret,
+      }
+    })
+    .then(function (response) {
+      console.log("Success! https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash)
+      return {
+        success: true,
+        pinataUrl: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+      };
+    })
+    .catch(function (error) {
+      console.log("Failure! Pinata did not pin " + error)
+      return {
+        success: false,
+        message: error.message,
+      }
+
+    });
 };
