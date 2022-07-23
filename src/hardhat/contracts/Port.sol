5cb4651600212eee6943d25db9805ac7e296279b
Nick Confrey <nickconfrey@earthlink.net>
2022-07-23 23:57:17 +0200

    first draft of Port smart contract to issue new lisences from any NFT

---
+++ src/hardhat/contracts/Port.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract Port is ReentrancyGuard {
+  using Counters for Counters.Counter;
+  Counters.Counter private _nftsSold;
+  Counters.Counter private _nftCount;
+  uint256 public LISTING_FEE = 0.0001 ether;
+  address payable private _marketOwner;
+  mapping(uint256 => NFT) private _idToNFT;
+  struct NFT {
+    address nftContract;
+    uint256 tokenId;
+    address payable creator;
+    address payable user;
+    uint256 price;
+    bool listed;
+  }
+  event NFTListed(
+    address nftContract,
+    uint256 tokenId,
+    address creator,
+    address user,
+    uint256 price
+  );
+  event LicenseCreated(
+    address nftContract,
+    uint256 tokenId,
+    address creator,
+    address user,
+    uint256 price
+  );
+
+  // Create license from NFT
+  function createLicense(address _nftContract, uint256 _tokenId) public payable nonReentrant {
+    NFT storage nft = _idToNFT[_tokenId];
+    require(msg.value >= nft.price, "Not enough ether to cover asking price");
+
+    address payable buyer = payable(msg.sender);
+    payable(nft.seller).transfer(msg.value);
+    IERC721(_nftContract).transferFrom(address(this), buyer, nft.tokenId);
+    _marketOwner.transfer(LICENSE_FEE);
+    nft.owner = buyer;
+    nft.listed = false;
+
+    _nftsSold.increment();
+    emit LicenseCreated(_nftContract, nft.tokenId, nft.seller, buyer, msg.value);
+  }
+
+  function getLicenseFee() public view returns (uint256) {
+    return LISTING_FEE;
+  }
+
+  function getMyNfts() public view returns (NFT[] memory) {
+    uint nftCount = _nftCount.current();
+    uint myNftCount = 0;
+    for (uint i = 0; i < nftCount; i++) {
+      if (_idToNFT[i + 1].owner == msg.sender) {
+        myNftCount++;
+      }
+    }
+
+    NFT[] memory nfts = new NFT[](myNftCount);
+    uint nftsIndex = 0;
+    for (uint i = 0; i < nftCount; i++) {
+      if (_idToNFT[i + 1].owner == msg.sender) {
+        nfts[nftsIndex] = _idToNFT[i + 1];
+        nftsIndex++;
+      }
+    }
+    return nfts;
+  }
+
+  function getMyListedNfts() public view returns (NFT[] memory) {
+    uint nftCount = _nftCount.current();
+    uint myListedNftCount = 0;
+    for (uint i = 0; i < nftCount; i++) {
+      if (_idToNFT[i + 1].seller == msg.sender && _idToNFT[i + 1].listed) {
+        myListedNftCount++;
+      }
+    }
+
+    NFT[] memory nfts = new NFT[](myListedNftCount);
+    uint nftsIndex = 0;
+    for (uint i = 0; i < nftCount; i++) {
+      if (_idToNFT[i + 1].seller == msg.sender && _idToNFT[i + 1].listed) {
+        nfts[nftsIndex] = _idToNFT[i + 1];
+        nftsIndex++;
+      }
+    }
+    return nfts;
+  }
+}
